6337709d67e5e75fa3e036e75bcf1edd0bb3c3f9
Victor Burkouski <v.burkouski@nord-soft.com>
2016-07-02 20:05:51 +0300

    added gulp-connect, opn and startDevServer task

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,31 @@
 var gulp = require('gulp');
+var connect = require('gulp-connect');
+var opn = require('opn');
+
+var devPort = 8000;
+var livePort = 8001;
+var devServer = 'http://localhost:'+devPort+'/';
+var liveServer = 'http://localhost:'+livePort+'/';
+
+
+gulp.task('default', defaultTask);
+gulp.task('startDevServer', startDevServer);
+
+function defaultTask() {
+    gulp.start([
+        // 'compileStylus',
+        // 'compileJade',
+        // 'setWatcher',
+        'startDevServer'
+    ]);
+}
+
+function startDevServer() {
+    connect.server({
+        port: 8000,
+        root: './src',
+        livereload: true
+    });
+    opn(devServer)
+}
+
